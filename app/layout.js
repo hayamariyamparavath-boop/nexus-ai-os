83d8091205e380dd5070d1046a11eeb7f6757bcd
hayamariyamparavath-boop <haya.mariyam.paravath@gmail.com>
2026-04-10 21:32:26 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'NEXUS AI OS',
+  description: 'AI Powered OS Interface',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
